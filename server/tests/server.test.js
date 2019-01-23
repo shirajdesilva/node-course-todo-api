2b38b4d8ef6f1f2a446e3ee312bb139120872e67
Shiraj De Silva <shiraj.desilva@gmail.com>
2019-01-23 18:25:55 +1100

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -279,6 +279,9 @@ describe ('POST /users', () => {
             users.findOne({email}).then( (user) => {
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
+                done();
+            }).catch ( (e) => {
+                done(e);
             })
         });
     })
@@ -309,5 +312,67 @@ describe ('POST /users', () => {
 
             // })
             .end(done)
+    })
+})
+
+
+
+describe('POST /users/login', () => {
+
+    it ('should login user and return auth token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password,
+        })
+        .expect(200)
+        .expect ( (res) => {
+            expect ( (res.header['x-auth']).toExist())
+
+        })
+        .end( (err,res) => {
+            if (err) {
+                return done(err)
+            }
+
+            User.findById(users[1]._id)
+            .then ( (user) => {
+                expect(user.tokens[0].toInclude({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                }))
+                done();
+            }).catch ( (e) => {
+                done(e);
+            })
+        })
+    })
+
+    it ('should reject invalid login', (done) => {
+
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password + '1',
+        })
+        .expect(400)
+        .expect ( (res) => {
+            expect ( (res.header['x-auth'].toNotExist() ))
+        })
+        .end( (err, res) => {
+            if (err) {
+                return done(err)
+            }
+
+            User.findByID(users[1]._id).then ((user) => {
+                expect(user.toke.length).toBe(0);
+                done();
+            }).catch ( (e)=> {
+                done(e);
+            })
+        })
+
     })
 })
